394dd7c9a89c1804274270cfb85ece3093e8cc20
Ansh Kumar Singh <3000ansh2004@gmail.com>
2026-02-07 03:49:45 +0530

    Config: Allow global CORS for deployment

--- src/main/java/com/teamresource/manager/DashboardController.java
+++ src/main/java/com/teamresource/manager/DashboardController.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173") // Allow React to access this
+@CrossOrigin(origins = "*") // Allow React to access this
 public class DashboardController {
 
     private final EmployeeRepository employeeRepository;
